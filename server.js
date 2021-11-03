ed099b33c5a72b25b6f0f71ba79e51341418fac4
YaochenS <ycshen@live.unc.edu>
2021-11-02 23:41:13 -0400

    e

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/user", (req, res) => {	
-	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)').run(req.body.user);
+	const stmt = db.prepare('INSERT INTO userinfo (user, pass) VALUES (?, ?)').run('Joey', 2);
 	res.status(201).json(stmt);
 });
 
